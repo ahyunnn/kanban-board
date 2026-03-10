2af10d19b324e66869eea1c283f4e2b4b042914a
ahyun <ahyun3089@gmail.com>
2026-03-10 20:51:51 +0900

    feat: create kanban board project

---
+++ script.js
@@ -0,0 +1,5 @@
+let tasks = [
+  { id: 1, text: "Study JavaScript", status: "todo" },
+  { id: 2, text: "Build a Kanban Board", status: "in-progress" },
+  { id: 3, text: "Test the Application", status: "done" },
+];
